df7a17f7344768c89d5f2c9fcd190ad78a16c6a3
LavanyaLahari811 <lavanyalaharik2003@gmail.com>
2023-12-23 15:03:09 +0530

    noButtonIsDone

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const noButton=document.querySelector('#no');
+const yesButton = document.querySelector("#yes");
+const noButton = document.querySelector("#no");
+
+yesButton.addEventListener("click", () => {
+  alert("Great! I like you too!");
+});
+
+noButton.addEventListener("click", () => {
+  var randomX = Math.floor(Math.random() * 100) - 50;
+  var randomY = Math.floor(Math.random() * 100) - 50;
+
+  noButton.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
+});
